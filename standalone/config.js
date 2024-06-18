c5353eade9425ab140ffb8718d499d995d41e0a1
John Candles <5869655+liska881@users.noreply.github.com>
2024-06-18 12:10:19 +0200

    Add files via upload

---
+++ standalone/config.js
@@ -0,0 +1,11 @@
+var config = {
+ url : {
+  configuration: 'standalone/dynmap_config.json?_={timestamp}',
+  update: 'standalone/dynmap_{world}.json?_={timestamp}',
+  sendmessage: 'standalone/sendmessage.php',
+  login: 'standalone/login.php',
+  register: 'standalone/register.php',
+  tiles: 'https://storage.googleapis.com/stara-dynmapa/tiles/',
+  markers: 'tiles/'
+ }
+};
